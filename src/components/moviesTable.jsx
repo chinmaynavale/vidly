dac6c3548b84ca0cb9e77e91287ca6fa80a28dd3
Chinmay Navale <chinmaymnavale@gmail.com>
2020-10-15 16:19:30 +0530

    Code Refactoring: Extracted Table component from MoviesTable component and moved TableHeader and TableBody components into Table component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import TableHeader from '../components/common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 import Like from './common/like';
 
 class MoviesTable extends Component {
@@ -29,17 +28,15 @@ class MoviesTable extends Component {
   ];
 
   render() {
-    const { movies, onSort, sortColumn } = this.props;
+    const { onSort, sortColumn, movies } = this.props;
 
     return (
-      <table className='table'>
-        <TableHeader
-          columns={this.columns}
-          onSort={onSort}
-          sortColumn={sortColumn}
-        />
-        <TableBody columns={this.columns} data={movies} />
-      </table>
+      <Table
+        columns={this.columns}
+        onSort={onSort}
+        sortColumn={sortColumn}
+        data={movies}
+      />
     );
   }
 }
