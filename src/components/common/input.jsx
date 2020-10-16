7660d0a87fea9d3554062447dc89a15d93600043
Chinmay Navale <chinmaymnavale@gmail.com>
2020-10-16 21:06:13 +0530

    Extracted reusable Form component from LoginForm component and did some code refactoring

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
 const Input = props => {
-  const { name, value, label, type, error, onChange } = props;
+  const { name, label, error, ...rest } = props;
 
   return (
     <div className='form-group'>
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        name={name}
-        id={name}
-        type={type}
-        className='form-control'
-      />
+      <input {...rest} name={name} id={name} className='form-control' />
+
       {error && <div className='alert alert-danger'>{error}</div>}
     </div>
   );
