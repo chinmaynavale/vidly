d3649db7788247ef0c3580404ad5ffa58a478686
Chinmay Navale <chinmaymnavale@gmail.com>
2020-10-17 13:34:06 +0530

    Implemented Register Form component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 import MovieForm from './components/movieForm';
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <NavBar />
       <main className='container'>
         <Switch>
+          <Route path='/register' component={RegisterForm} />
           <Route path='/login' component={LoginForm} />
           <Route path='/movies/:id' component={MovieForm} />
           <Route path='/movies' component={Movies} />
